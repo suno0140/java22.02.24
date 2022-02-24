d174eb63949df5bcc0ee3a68160bd1ea3c8ed83f
suno <vlfxhd15@naver.com>
2022-02-24 12:16:25 +0900

    first commit

---
+++ src/chap01/CodingTip.java
@@ -0,0 +1,28 @@
+package chap01;
+
+public class CodingTip {
+    // hint : ctrl + space
+	
+	// main 메소드 코드 단축 키워드 : main
+	public static void main(String[] args) {
+		
+		// 자동코드 포맷팅 : ctrl + shift + f
+		
+		// 콘솔 출력
+		System.out.println("출력할 텍스트");
+		
+		// 콘솔 출력 코드 단축키워드 : sysout, sout (ctrl + space)
+		System.out.println("출력할 텍스트");
+		System.out.println("출력할 텍스트");
+		
+		// 실행 : ctrl + f11
+		
+		// 주석 : ctrl + /
+		
+		// 창 크기 변경 : ctrl + m
+		
+		// 새파일 : ctrl + n
+		
+		// 저장 : ctrl + s
+	}
+}
