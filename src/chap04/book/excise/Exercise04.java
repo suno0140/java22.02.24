249cfac074c5b7a6a2aa460745fc1a89d963c25c
suno <vlfxhd15@naver.com>
2022-03-03 17:06:14 +0900

    while

---
+++ src/chap04/book/excise/Exercise04.java
@@ -0,0 +1,17 @@
+package chap04.book.excise;
+
+public class Exercise04 {
+	public static void main(String[] args) {
+
+		boolean more = true;
+		while (more) {
+			int num1 = (int) (Math.random() * 6) + 1;
+			int num2 = (int) (Math.random() * 6) + 1;
+
+			System.out.println("(" + num1 + "," + num2 + ")");
+			if ((num1 + num2) == 5) {
+				more = false;
+			}
+		}
+	}
+}
---
+++ src/chap04/lecture/whileloop/C02DoWhile.java
@@ -0,0 +1,13 @@
+package chap04.lecture.whileloop;
+
+public class C02DoWhile {
+	public static void main(String[] args) {
+		System.out.println("stmt 1");
+		
+		do {
+			System.out.println("stmt 2");
+			System.out.println("stmt 3");
+		} while (false);
+		System.out.println("stmt 4");
+	}
+}
